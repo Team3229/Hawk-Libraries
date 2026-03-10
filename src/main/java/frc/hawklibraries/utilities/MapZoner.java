2485da313d9234c2dc0cc79a4f9efb0d59b46b3a
derpman046 <aidanh2016@outlook.com>
2026-03-10 16:12:03 -0400

    I forgot to add the wpi values

--- src/main/java/frc/hawklibraries/utilities/MapZoner.java
+++ src/main/java/frc/hawklibraries/utilities/MapZoner.java
@@ -2,6 +2,9 @@ package frc.hawklibraries.utilities;
 
 import java.util.HashMap;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+
 public class MapZoner {
     public HashMap<String, Zone> zoneMap;
 
@@ -24,5 +27,44 @@ public class MapZoner {
         Zone zone = zoneMap.get(zoneName);
 
         return zone.getShape().contains(botX, botY);
+    }
+
+    /*
+     * Returns if the rectangle is in a registered zone
+     */
+    public boolean check(String zoneName, double botX, double botY, double width, double height) {
+        Zone zone = zoneMap.get(zoneName);
+
+        return zone.getShape().contains(botX, botY, width, height);
+    }
+
+    /*
+     * Returns if the x and y cords are in a registered zone
+     */
+    public boolean check(String zoneName, Pose2d pos) {
+        return check(zoneName, pos.getX(), pos.getY());
+    }
+
+    /*
+     * Returns if the rectangle is in a registered zone
+     */
+    public boolean check(String zoneName, Pose2d pos, double width, double height) {
+        return check(zoneName, pos.getX(), pos.getY(), width, height);
+    }
+
+    /*
+     * Returns if the x and y cords are in a registered zone
+     * Warning!!! This gets rid of angles.
+     */
+    public boolean check(String zoneName, Translation2d pos) {
+        return check(zoneName, pos.getX(), pos.getY());
+    }
+
+    /*
+     * Returns if  the rectangle is in a registered zone
+     * Warning!!! This gets rid of angles.
+     */
+    public boolean check(String zoneName, Translation2d pos, double width, double height) {
+        return check(zoneName, pos.getX(), pos.getY(), width, height);
     }
 }
