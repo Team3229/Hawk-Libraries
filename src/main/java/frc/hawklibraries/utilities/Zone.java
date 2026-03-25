0f653fb4eee1b6c40361683c281c039d5f89b6e9
derpman046 <aidanh2016@outlook.com>
2026-03-25 08:35:17 -0400

    bored +1

--- src/main/java/frc/hawklibraries/utilities/Zone.java
+++ src/main/java/frc/hawklibraries/utilities/Zone.java
@@ -10,7 +10,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
 /**
- * Used to store shape data in a easy-to-learn way. Used with MapZoner. 
+ * Used to store shape data in a easy-to-learn way. Used best with MapZoner. 
  * Does not support angles. 
  * Suggestion: use top left DrawTypes. 
  */
