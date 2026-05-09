fd6aeb29cab65b843c1214ff1d70feae1aa5a7b7
Kara Campbell <j5cc0109@gmail.com>
2026-05-09 17:02:12 -0400

    finishing touches and PID setter
    Co-authored-by: derpman046

--- src/main/java/frc/hawklibraries/baseTalons/TalonConfig.java
+++ src/main/java/frc/hawklibraries/baseTalons/TalonConfig.java
@@ -14,6 +14,11 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.units.measure.Velocity;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 
 public class TalonConfig {
@@ -44,6 +49,11 @@ public class TalonConfig {
 
     private double currentLimit = 40;
 
+    private Sendable pidSetter;
+
+    /**
+     * Sets the P, I, D, V, A, S, G values without setting the values on a motor.
+     */
     public TalonConfig setPIDVASG(double p, double i, double d, double v, double a, double s, double g) {
         this.p = p;
         this.i = i;
@@ -52,20 +62,116 @@ public class TalonConfig {
         this.a = a;
         this.s = s;
         this.g = g;
-
+        
         return this;
     }
 
+    /**
+     * Sets the P, I, D, V, A, S, G values with setting the values on a motor.
+     */
+    public TalonConfig setPIDVASG(double p, double i, double d, double v, double a, double s, double g, TalonFX talon) {
+        this.p = p;
+        this.i = i;
+        this.d = d;
+        this.v = v;
+        this.a = a;
+        this.s = s;
+        this.g = g;
+        setSlot0();
+        applyConfig(talon);
+        
+        return this;
+    }
+
+    /**
+     * Sets the P, I, D values with setting the values on a motor.
+     */
+    public TalonConfig setPID(double p, double i, double d, TalonFX talon) {
+        return setPIDVASG(p, i, d, this.v, this.a, this.s, this.g, talon);
+    }
+    
+    /**
+     * Sets the V, A, S, G values with setting the values on a motor.
+     */
+    public TalonConfig setVASG(double v, double a, double s, double g, TalonFX talon) {
+        return setPIDVASG(this.p, this.i, this.d, v, a, s, g, talon);
+    }
+
+    /**
+     * Sets the P, V, S values with setting the values on a motor.
+     */
+    public TalonConfig setPVS(double p, double v, double s, TalonFX talon) {
+        return setPIDVASG(p, this.i, this.d, v, this.a, s, this.g, talon);
+    }
+
+    /**
+     * Sets the P, I, D values without setting the values on a motor.
+     */
     public TalonConfig setPID(double p, double i, double d) {
         return setPIDVASG(p, i, d, this.v, this.a, this.s, this.g);
     }
     
+    /**
+     * Sets the V, A, S, G values without setting the values on a motor.
+     */
     public TalonConfig setVASG(double v, double a, double s, double g) {
         return setPIDVASG(this.p, this.i, this.d, v, a, s, g);
     }
 
+    /**
+     * Sets the P, V, S values with setting the values on a motor.
+     */
     public TalonConfig setPVS(double p, double v, double s) {
-        return setPIDVASG(p, this.i, this.d, v, this.a, s, this.g);   
+        return setPIDVASG(p, this.i, this.d, v, this.a, s, this.g);
+    }
+
+    /**
+     * Sets the P value to the motor
+     */
+    public TalonConfig setP(double p, TalonFX talon) {
+        return setPID(p, this.i, this.d, talon);
+    }
+
+    /**
+     * Sets the I value to the motor
+     */
+    public TalonConfig setI(double i, TalonFX talon) {
+        return setPID(this.p, i, this.d, talon);   
+    }
+
+    /**
+     * Sets the D value to the motor
+     */
+    public TalonConfig setD(double d, TalonFX talon) {
+        return setPID(this.p, this.i, d, talon);   
+    }
+
+    /**
+     * Sets the V value to the motor
+     */
+    public TalonConfig setV(double v, TalonFX talon) {
+        return setVASG(v, this.a, this.s, this.g, talon);   
+    }
+
+    /**
+     * Sets the A value to the motor
+     */
+    public TalonConfig setA(double a, TalonFX talon) {
+        return setVASG(this.v, a, this.s, this.g, talon);   
+    }
+
+    /**
+     * Sets the S value to the motor
+     */
+    public TalonConfig setS(double s, TalonFX talon) {
+        return setVASG(this.v, this.a, s, this.g, talon);   
+    }
+
+    /**
+     * Sets the G value to the motor
+     */
+    public TalonConfig setG(double g, TalonFX talon) {
+        return setVASG(this.v, this.a, this.s, g, talon);   
     }
 
     public TalonConfig setInvertedValue(InvertedValue forwardDirection) {
@@ -159,7 +265,7 @@ public class TalonConfig {
         return this;
     }
 
-    public TalonConfig withMotionMagicConfigurator(){
+    public TalonConfig withMotionMagicConfigurator() {
         motionMagicConfigs = new MotionMagicConfigs()
             .withMotionMagicCruiseVelocity(armVelocity)
             .withMotionMagicAcceleration(armVelocity * 2)
@@ -168,6 +274,35 @@ public class TalonConfig {
 
         return this;
     }
+
+    public TalonConfig applyConfigurators() {
+        withMotionMagicConfigurator();
+        withCurrentLimitsConfigurator();
+        withFeedbackConfigurator();
+        withMotorOutputConfigurator();
+        withVoltageConfigurator();
+        setSlot0();
+
+        return this;
+    }
+
+    public TalonConfig withPIDSendable(String name, TalonFX talon) {
+        pidSetter = new Sendable() {
+		    @Override
+			public void initSendable(SendableBuilder builder) {
+				builder.addDoubleProperty("P", () -> p, (P) -> setP(P, talon));
+				builder.addDoubleProperty("I", () -> i, (I) -> setI(I, talon));
+				builder.addDoubleProperty("D", () -> d, (D) -> setD(D, talon));
+				builder.addDoubleProperty("V", () -> v, (V) -> setV(V, talon));
+				builder.addDoubleProperty("A", () -> a, (A) -> setA(A, talon));
+				builder.addDoubleProperty("S", () -> s, (S) -> setS(S, talon));
+				builder.addDoubleProperty("G", () -> g, (G) -> setG(G, talon));
+			}
+		};
+		SmartDashboard.putData(name + "PID", pidSetter);
+        return this;
+    }
+
 
     public void applyConfig(TalonFX motor) {
         motor.getConfigurator().apply(talonConfig);
---
+++ src/main/java/frc/hawklibraries/baseTalons/examples/ExamplePrecise.java
@@ -0,0 +1,38 @@
+package frc.hawklibraries.baseTalons.examples;
+
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.hawklibraries.baseTalons.PreciseTalon;
+import frc.hawklibraries.baseTalons.TalonConfig;
+
+public class ExamplePrecise extends SubsystemBase {
+    // i am pretending that this is a subsystem, albiet a small one
+    public PreciseTalon talon;
+    public TalonConfig config;
+
+    public ExamplePrecise() {
+        super();
+        config = new TalonConfig()
+        .setPIDVASG(10, 1, 4, 45, 190, 0.0001, 85)
+            .setSensorToMechanismRatio(1000)
+            .setCurrentLimit(40) // the default is 40 so it won't be changed
+            .setMaxVoltage(12) // the default is 12 so it won't be changed
+            .setNeutralModeValue(NeutralModeValue.Coast)
+            .setInvertedValue(InvertedValue.CounterClockwise_Positive)
+            .applyConfigurators()
+            .withPIDSendable("spin", talon.motor);
+
+        talon = new PreciseTalon(0, CANBus.roboRIO(), config);
+    }
+
+    public Command moveTo(Angle angle) {
+        moveTo(angle).addRequirements(this); // tbh idk if this is working, but i'd like it too
+        return talon.rotateTo(angle);
+    }
+
+}
